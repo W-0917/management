a6bc7e9591524b25278d0448fc8214012b19c758
王纪清 <2663439864@qq.com>
2020-12-14 19:15:38 +0800

    王纪清提交后台管理

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,72 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <el-container>
+      <el-header class="nav" v-show="showEdit">
+        <h1>UNI - ADMIN</h1>
+        <div class="right">
+          <el-menu
+            :default-active="activeIndex"
+            class="el-menu-demo"
+            mode="horizontal"
+            background-color="#545c64"
+            text-color="#fff"
+            active-text-color="#ffd04b"
+            router
+          >
+            <el-menu-item index="/home">首页</el-menu-item>
+            <el-menu-item index="/shop">商品</el-menu-item>
+            <el-menu-item index="/order">订单</el-menu-item>
+            <el-menu-item index="/member">会员</el-menu-item>
+            <el-menu-item index="/set">设置</el-menu-item>
+          </el-menu>
+          <el-avatar></el-avatar>
+          <span type="primary" @click="gotologin">退出登录</span>
+        </div>
+      </el-header>
+      <router-view />
+    </el-container>
   </div>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      activeIndex: "1",
+      showEdit: false,
+    };
+  },
+  methods: {
+    gotologin() {
+      this.$message("退出登录");
+      localStorage.removeItem("token");
+      this.$router.push("/");
+    },
+  },
+  watch: {
+    $route: {
+      handler(newval) {
+        if(newval.path=='/'){
+          this.showEdit=false
+        }else{
+             this.showEdit=true
+        }
+      },
+    },
+  },
+};
+</script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+.nav {
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background-color: #545c64;
+  color: white;
+  .right {
+    display: flex;
+    align-items: center;
   }
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -5,6 +5,16 @@ import store from './store'
 
 Vue.config.productionTip = false
 
+import axios from 'axios';
+Vue.prototype.$axios=axios
+
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+Vue.use(ElementUI);
+
+import http from "./http/http.js";
+Vue.prototype.$http = http;
+
 new Vue({
   router,
   store,
